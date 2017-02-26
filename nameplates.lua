16e2fa8565bcc5775525dacdd489b8df98534292
Michael Fuglsang <Michael Fuglsang>
2017-02-26 20:06:23 +0100

    Fixed typo

--- nameplates.lua
+++ nameplates.lua
@@ -161,7 +161,7 @@ local function FilterQuestTexts(arrangedTexts)
 		if not addedWorldQuests[arrangedTexts[i].text] and not addedInstanceQuests[arrangedTexts[i].text] then --Filter recurring world and instance quests, removing quests from party members. First one should be ours
 			if arrangedTexts[i].type == "worldQuestTitle" then
 				addedWorldQuests[arrangedTexts[i].text] = true
-			elseif arrangedTexts[i].type = "instanceTitle" then
+			elseif arrangedTexts[i].type == "instanceTitle" then
 				addedInstanceQuests[arrangedTexts[i].text] = true
 			end
 
