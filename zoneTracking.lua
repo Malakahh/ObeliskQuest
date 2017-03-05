42f9c80df125edd8df2ff5a9a9631ec790fb9680
Michael Fuglsang <Michael Fuglsang>
2017-03-05 23:04:57 +0100

    Fixed nil error

--- zoneTracking.lua
+++ zoneTracking.lua
@@ -31,7 +31,7 @@ local function TrackByZone()
 		local title, _, _, isHeader, _, _, _, questID = GetQuestLogTitle(i)
 
 		if isHeader then
-			if title == currentMapName or tContains(ns.ZoneNameSubstitutions[currentMapName], title) then
+			if title == currentMapName or (ns.ZoneNameSubstitutions[currentMapName] and tContains(ns.ZoneNameSubstitutions[currentMapName], title)) then
 				watchQuest = true
 			elseif watchQuest then
 				break
