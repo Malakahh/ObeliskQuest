d0411644e3b5b080191c47b8428459403cd9ed4c
Michael Fuglsang <Michael Fuglsang>
2017-03-06 15:41:31 +0100

    Fixed bug where rewards icon failed to hide if gossip dialog closed without actually handing in the quest

--- rewards.lua
+++ rewards.lua
@@ -10,6 +10,7 @@ frame:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end
 frame:RegisterEvent("QUEST_COMPLETE")
 frame:RegisterEvent("QUEST_DETAIL")
 frame:RegisterEvent("QUEST_TURNED_IN")
+frame:RegisterEvent("QUEST_FINISHED")
 
 local function GetMax(t)
 	if #t == 0 then return nil, nil end
@@ -48,5 +49,9 @@ function frame:QUEST_TURNED_IN()
 end
 
 function frame:QUEST_DETAIL()
+	self:Hide()
+end
+
+function frame:QUEST_FINISHED()
 	self:Hide()
 end
